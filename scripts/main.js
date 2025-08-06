f1ae7da326789e548ba992cfc703d40ae73144b2
itadori <itadoriyuji414@gmail.com>
2025-08-06 20:29:20 +0530

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+// Smooth scroll for internal page links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Simple console log to confirm JS loaded
+console.log('Main JS Loaded Successfully');
